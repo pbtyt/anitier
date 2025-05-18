69abf120dd26223a643c9b41bf1b3b9ef55bc5f2
pbtyt <darkyt9@gmail.com>
2025-05-19 00:55:03 +0300

    Add Criteria Tab

---
+++ src/_pages/cardPage/ui/CardPage/CriteriaTab/CriteriaTab.tsx
@@ -0,0 +1,5 @@
+import styles from './CriteriaTab.module.scss';
+
+export function CriteriaTab({ cardId }: { cardId: string }) {
+	return <section className={styles.wrapper}>Criteria</section>;
+}
--- src/_pages/cardPage/ui/CardPage/TabsSection/TabSection.tsx
+++ src/_pages/cardPage/ui/CardPage/TabsSection/TabSection.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import { UpTabs } from '@/shared/ui/Tabs';
+import { CriteriaTab } from '../CriteriaTab/CriteriaTab';
 import { EpisodeTab } from '../EpisodeTab/EpisodeTab';
-
+import styles from './TabSection.module.scss';
 export function TabSection({ cardId }: { cardId: string }) {
 	//TODO: Second Tab (Edit/Create/Delete Criteria)
 	return (
-		<UpTabs>
+		<UpTabs className={styles.tabWrapper}>
 			<UpTabs.TabsHeader>
 				<UpTabs.Tab title='Эпизоды' />
 				<UpTabs.Tab title='Редактирование критериев' />
@@ -16,7 +17,9 @@ export function TabSection({ cardId }: { cardId: string }) {
 			<UpTabs.Content viewIndex={0}>
 				<EpisodeTab cardId={cardId} />
 			</UpTabs.Content>
-			<UpTabs.Content viewIndex={1}>SECOND</UpTabs.Content>
+			<UpTabs.Content viewIndex={1}>
+				<CriteriaTab cardId={cardId} />
+			</UpTabs.Content>
 			<UpTabs.Content viewIndex={2}>THIRD</UpTabs.Content>
 		</UpTabs>
 	);
